07b5dbb2c0b5b481b14491e2fed0626bca5dd2e2
Ghadier-Alenezi <Ghadier.Mutar.elq.js@tuwaiq.edu.sa>
2022-02-17 14:46:47 +0300

    update

--- lab.py
+++ lab.py
@@ -1,6 +1,16 @@
-def vowelsCounter(word:str)->int:
+def vowelsCounter(word:str, index =0, counter=0)->int:
     vowels = ["a","e","i","o","u"]
-    counter = [x for x in word.lower() if x in vowels]
-    return len(counter)
-
+    word = word.lower()
+    if index < len(word):
+        for c in word:
+            if c in vowels:
+                counter+=1
+                index+=1
+                vowelsCounter(word, index, counter)
+            else:
+                index+=1
+                vowelsCounter(word, index, counter)
+        return counter
+    else:
+        return
 print(vowelsCounter("I love python"))
